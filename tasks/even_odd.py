08e8d3403f29f5fe151fd78281f4fbe7c1dd6c9b
islamKadeev <kadeevkgeu@gmail.com>
2023-02-13 18:15:37 +0300

    even_odd task done

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -9,4 +9,15 @@ def even_odd(arr: list[int]) -> float:
     Пример:
     even_odd([1, 2, 3, 4, 5]) == 0.8889
     """
-    raise NotImplementedError
+    if not arr:
+        return 0
+
+    even_list= list(filter(lambda element: element % 2 == 0, arr))
+    odd_list = list(filter(lambda element: element % 2 != 0, arr))
+
+    if not odd_list:
+        return 0
+    if not even_list:
+        return 0
+
+    return sum(even_list) / sum(odd_list)
