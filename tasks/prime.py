c597b9232f984ef20fa7ef70717e2eb2ad228514
islamKadeev <kadeevkgeu@gmail.com>
2023-02-13 12:47:06 +0300

    prime task done

--- tasks/prime.py
+++ tasks/prime.py
@@ -4,7 +4,14 @@ __all__ = (
 
 
 def is_prime(number: int) -> bool:
-    """
-    Функция должна вернуть True если число является простым, иначе - False
-    """
-    raise NotImplementedError
+    dividers: int = 0
+
+    if number == 1 or number == 0:
+        return False
+
+    for num in range(1, number // 2 + 1):
+        if number % num == 0:
+            dividers += 1
+            if dividers > 1:
+                return False
+    return True
