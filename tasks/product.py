f59440d6cbc00b921e248d8dcf0d46cb732e5458
islamKadeev <kadeevkgeu@gmail.com>
2023-02-13 18:24:07 +0300

    product task done

--- tasks/product.py
+++ tasks/product.py
@@ -1,4 +1,5 @@
-from typing import Any
+from typing import Any, List
+import itertools
 
 __all__ = (
     'cartesian_product',
@@ -10,4 +11,12 @@ def cartesian_product(arr1: list[Any], arr2: list[Any]) -> list:
     Должна возвращать все пары элементы двух массивов:
     cartesian_product([1, 2], [3, 4]) == [(1, 3), (1, 4), (2, 3), (2, 4)]
     """
-    raise NotImplementedError
+    if not arr1 or not arr2:
+        return []
+
+    result: List = []
+
+    for element in itertools.product(arr1, arr2):
+        result.append(element)
+
+    return result
