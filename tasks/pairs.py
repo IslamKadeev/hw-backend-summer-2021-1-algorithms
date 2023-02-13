0ab4d9cb5cb01be4883bffc4228f56fa3d7a525c
islamKadeev <kadeevkgeu@gmail.com>
2023-02-13 18:32:30 +0300

    pairs task done

--- tasks/pairs.py
+++ tasks/pairs.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 
 __all__ = (
     'corresponding_pairs',
@@ -10,4 +10,10 @@ def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any
     Функция должна возвращать соответствующие элементы двух массивов:
     corresponding_pairs([1, 2], [3, 4]) == [(1, 3), (2, 4)]
     """
-    raise NotImplementedError
+
+    if not arr1 or not arr2:
+        return []
+
+    result: List = list(zip(arr1, arr2))
+
+    return result
