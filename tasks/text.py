8ace7c784d9bc357eb28c18352e79896d823e57d
islamKadeev <kadeevkgeu@gmail.com>
2023-02-13 19:15:56 +0300

    text task done

--- tasks/text.py
+++ tasks/text.py
@@ -10,4 +10,26 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
-    raise NotImplementedError
+
+    if " ".join(text.split()) == " ":
+        return None, None
+
+    text = text.split()
+    sortedtext = sorted(text)
+
+    if len(sortedtext) <= 1:
+        return None, None
+    else:
+        shortest_word = sortedtext[0]
+        longest_word = sortedtext[-1]
+        for element in sortedtext:
+
+            if element.split().count(','):
+                element = "".join(element[:-1])
+
+            if len(element) > len(longest_word):
+                longest_word = element
+            if len(element) < len(shortest_word):
+                shortest_word = element
+
+    return shortest_word, longest_word
