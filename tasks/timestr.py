e145475bc18562d1db2e627bdf00583b9b0726cc
islamKadeev <kadeevkgeu@gmail.com>
2023-02-13 17:50:00 +0300

    timestr task done

--- tasks/timestr.py
+++ tasks/timestr.py
@@ -12,8 +12,75 @@ def seconds_to_str(seconds: int) -> str:
     3700 -> 01h01m40s
     93600 -> 01d02h00m00s
     """
-    raise NotImplementedError
+    if seconds >= 60 * 60 * 24:
+        day = seconds // (60 * 60 * 24)
+        seconds %= 60 * 60 * 24
+        hour = seconds // (60 * 60)
+        seconds %= 60 * 60
+        minute = seconds // 60
+        seconds %= 60
 
+        if day < 10:
+            day = '0' + str(day) + 'd'
+        else:
+            day = str(day) + 'd'
 
+        if hour < 10:
+            hour = '0' + str(hour) + 'h'
+        else:
+            hour = str(hour) + 'h'
 
+        if minute < 10:
+            minute = '0' + str(minute) + 'm'
+        else:
+            minute = str(minute) + 'm'
 
+        if seconds < 10:
+            seconds = '0' + str(seconds) + 's'
+        else:
+            seconds = str(seconds) + 's'
+
+        return day + hour + minute + seconds
+    elif seconds >= 60 * 60:
+        hour = seconds // (60 * 60)
+        seconds %= 60 * 60
+        minute = seconds // 60
+        seconds %= 60
+
+        if hour < 10:
+            hour = '0' + str(hour) + 'h'
+        else:
+            hour = str(hour) + 'h'
+
+        if minute < 10:
+            minute = '0' + str(minute) + 'm'
+        else:
+            minute = str(minute) + 'm'
+
+        if seconds < 10:
+            seconds = '0' + str(seconds) + 's'
+        else:
+            seconds = str(seconds) + 's'
+
+        return hour + minute + seconds
+    elif seconds >= 60:
+        minute = seconds // 60
+        seconds %= 60
+
+        if minute < 10:
+            minute = '0' + str(minute) + 'm'
+        else:
+            minute = str(minute) + 'm'
+
+        if seconds < 10:
+            seconds = '0' + str(seconds) + 's'
+        else:
+            seconds = str(seconds) + 's'
+
+        return minute + seconds
+    else:
+        if seconds < 10:
+            seconds = '0' + str(seconds) + 's'
+        else:
+            seconds = str(seconds) + 's'
+        return seconds
